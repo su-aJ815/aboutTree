e80f748fcb2794577017a7628d5db01a36ff0c43
suaj <rmdwjdmind@gmail.com>
2022-03-27 21:25:50 +0900

    :lipstick: Add SubImg
    Tree Img

--- src/pages/subpage.jsx
+++ src/pages/subpage.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import treeImg from "../images/treeImg.png";
+
+const SubImg = styled.div`
+  width: 40vw;
+  height: 40vh;
+  background-image: url(${treeImg});
+  background-position: center;
+  background-repeat: none;
+  background-size: cover;
+`;
 
 export const SubPage = () => {
-  return <></>;
+  return (
+    <>
+      <SubImg />
+    </>
+  );
 };
